910a456c2f78defc5ef5171c99c7021531cebe3e
waterbeen0530 <hange253@gmail.com>
2022-07-19 22:17:26 +0900

    [CREATE] Raccoon Extensions
    모든 img를 Raccoon img로 변경하는Google Extensions

---
+++ index.js
@@ -0,0 +1,5 @@
+let imgs = document.querySelectorAll("img");
+imgs.forEach((a, i) => {
+  a.src =
+    "https://coinpan.com/files/attach/images/198/115/002/214/a0eb50ac0d914d4e57ea103fb062fd56.jpeg";
+});
