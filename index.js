ee5b583965c2f5bbf9885d17efb967b45523943e
waterbeen0530 <hange253@gmail.com>
2022-07-19 22:48:50 +0900

    [UPDATE] Reload page every 0.1 second
    0.1초 간격으로 페이지 리로드

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let imgs = document.querySelectorAll("img");
-imgs.forEach((a, i) => {
-  a.src =
-    "https://coinpan.com/files/attach/images/198/115/002/214/a0eb50ac0d914d4e57ea103fb062fd56.jpeg";
-});
+setInterval(() => {
+  let imgs = document.querySelectorAll("img");
+  imgs.forEach((a, i) => {
+    a.src =
+      "https://coinpan.com/files/attach/images/198/115/002/214/a0eb50ac0d914d4e57ea103fb062fd56.jpeg";
+  });
+}, 100);
